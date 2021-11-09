f1166c757f9f8c80ed998813af0c502f46956158
Matan <matanp103@gmail.com>
2021-11-09 08:58:04 +0200

    Add Target Ctor

--- Engine/src/Target.java
+++ Engine/src/Target.java
@@ -1,12 +1,56 @@
+import schema.generated.GPUPTarget;
+import schema.generated.GPUPTargetDependencies;
+
+import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 public class Target {
 
     private String name;
     private PlaceInGraph place;
-    private Set<Target> requiredFor;
-    private Set<Target> dependsOn;
+    private Set<Target> requiredFor = new HashSet<>();
+    private Set<Target> dependsOn = new HashSet<>();
     private String info;
+
+
+    public Target(GPUPTarget target) {
+        this.name = target.getName();
+        this.info = target.getGPUPUserData();
+        for(GPUPTargetDependencies.GPUGDependency currDependency: target.getGPUPTargetDependencies().getGPUGDependency())
+        {
+            if(currDependency.getType().equals("requiredFor"))
+            {
+                //TODO: talk with Yael about this
+                this.requiredFor.add(new Target(currDependency.getValue()));
+            }
+            if(currDependency.equals("dependsOn"))
+            {
+                this.dependsOn.add(new Target(currDependency.getValue()));
+            }
+            if(this.requiredFor.isEmpty() && this.dependsOn.isEmpty())
+            {
+                this.place = PlaceInGraph.INDEPENDENT;
+            }
+            else if(this.requiredFor.isEmpty())
+            {
+                this.place = PlaceInGraph.ROOT;
+            }
+            else if(this.dependsOn.isEmpty())
+            {
+                this.place = PlaceInGraph.LEAF;
+            }
+            else
+            {
+                this.place = PlaceInGraph.MIDDLE;
+            }
+        }
+    }
+
+    public Target(String name)
+    {
+        this.name = name;
+    }
 
 
     public String getName() {
@@ -38,5 +82,18 @@ public class Target {
                 ", dependsOn=" + dependsOn +
                 ", info='" + info + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Target target = (Target) o;
+        return Objects.equals(name, target.name) && place == target.place && Objects.equals(requiredFor, target.requiredFor) && Objects.equals(dependsOn, target.dependsOn) && Objects.equals(info, target.info);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, place, requiredFor, dependsOn, info);
     }
 }
