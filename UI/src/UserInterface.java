b5bd8088953a3f19fd7c743e2f2532d9fb2016b8
Matan <matanp103@gmail.com>
2021-11-13 10:28:53 +0200

    UserInterface work

--- UI/src/UserInterface.java
+++ UI/src/UserInterface.java
@@ -12,11 +12,10 @@ public class UserInterface {
         this.engine = new SystemEngine();
     }
 
-    public void getInputFromUser(){
+    public void getInputFromUser() {
         boolean exit = false;
         Scanner in = new Scanner(System.in);
-        while (!exit)
-        {
+        while (!exit) {
             System.out.println("Please choose a number between 1-6:" +
                     "1. Read File." +
                     "2. Get information about Targets graph." +
@@ -26,7 +25,7 @@ public class UserInterface {
                     "6. Exit");
             try {
                 int choice = in.nextInt();
-                switch (choice){
+                switch (choice) {
                     case 1:
                         loadFile();
                         break;
@@ -38,10 +37,10 @@ public class UserInterface {
                         break;
                     case 4:
                         findPaths();
-                        //TODO: input target Name
+
                         break;
                     case 5:
-                        engine.activateTask();
+                        //engine.activateTask();
                         break;
                     case 6:
                         exit = true;
@@ -50,28 +49,28 @@ public class UserInterface {
                         System.out.println("Invalid choice, please try again");
                         break;
                 }
-            } catch(InputMismatchException e)
-            {
+            } catch (InputMismatchException e) {
                 System.out.println("Invalid Input: " + e.getMessage());
-            } catch (NoFileInSystemException e) {
-                e.printStackTrace();
-            } catch (TargetNotExistException e) {
-                e.printStackTrace();
-            } catch (DuplicateTargetsException e) {
-                e.printStackTrace();
-            } catch (InvalidDependencyException e) {
-                e.printStackTrace();
-            } catch (InvalidFileException e) {
-                e.printStackTrace();
-            } catch (DependencyConflictException e) {
-                e.printStackTrace();
             }
+//            catch (NoFileInSystemException e) {
+//                e.printStackTrace();
+//            } catch (TargetNotExistException e) {
+//                e.printStackTrace();
+//            } catch (DuplicateTargetsException e) {
+//                e.printStackTrace();
+//            } catch (InvalidDependencyException e) {
+//                e.printStackTrace();
+//            } catch (InvalidFileException e) {
+//                e.printStackTrace();
+//            } catch (DependencyConflictException e) {
+//                e.printStackTrace();
+//            }
 
 
         }
     }
 
-    private void findPaths(){
+    private void findPaths() {
         Scanner in = new Scanner(System.in);
         System.out.println("Please enter first Target's name:");
         String firstTargetName = in.next();
@@ -82,97 +81,124 @@ public class UserInterface {
         Collection<List<TargetDTO>> paths = null;
         try {
             paths = engine.getPaths(firstTargetName, secondTargetName, relation);
-            if(paths.isEmpty()){
-                System.out.println("There are not paths from target "+firstTargetName+ " to target " +secondTargetName);
-            }
-            else{
+            if (paths.isEmpty()) {
+                System.out.println("There are not paths from target " + firstTargetName + " to target " + secondTargetName);
+            } else {
                 printPaths(paths);
             }
-            //todo: write exception messages to the console
         } catch (NoFileInSystemException e) {
-            e.printStackTrace();
+            System.out.println("A file is not loaded to the system yet");
         } catch (TargetNotExistException e) {
-            e.printStackTrace();
+            System.out.println("The target" + e.getName() + " does not exist");
         } catch (InvalidDependencyException e) {
-            e.printStackTrace();
+            System.out.println("The dependency: " + e.getDependency() + " is not supported in the system");
         }
     }
 
     private void printPaths(Collection<List<TargetDTO>> paths) {
-        for(List<TargetDTO> path : paths){
+        for (List<TargetDTO> path : paths) {
             String currPath = "Path: ";
-            for(int i = 0; i < path.size()-1 ; ++i){
+            for (int i = 0; i < path.size() - 1; ++i) {
                 currPath += (path.get(i).getName() + "->");
             }
-            currPath += path.get(path.size()-1).getName();
+            currPath += path.get(path.size() - 1).getName();
             System.out.println(currPath);
         }
     }
 
-    //TODO: insert catch exceptions to functions
-    private void loadFile() throws DuplicateTargetsException, InvalidFileException, InvalidDependencyException, TargetNotExistException, DependencyConflictException {
-        System.out.println("Please enter the full path of file:");
-        Scanner in = new Scanner(System.in);
-        String path = in.next();
-        engine.readFile(path);
-    }
-
-
-    private void printGraphInfo() throws NoFileInSystemException {
-        GraphDTO graphDTO= engine.getGraphDTO();
-        System.out.println("Number of targets:" + graphDTO.getNumOfTargets());
-        System.out.println("Number of Leaves:" + graphDTO.getNumOfTargetsInPlace(PlaceInGraph.LEAF)+
-                "Number of Middles:" +graphDTO.getNumOfTargetsInPlace(PlaceInGraph.MIDDLE) +
-                "Number of Roots:" +graphDTO.getNumOfTargetsInPlace(PlaceInGraph.ROOT) +
-                "Number of Independents:" +graphDTO.getNumOfTargetsInPlace(PlaceInGraph.INDEPENDENT));
-    }
-
-
-
-    private void printTarget() throws NoFileInSystemException {
-        GraphDTO graphDTO = engine.getGraphDTO();
+    private void loadFile() {
         boolean finish = false;
-        while(!finish) {
-            System.out.println("Please enter the target's name:(to return to main menu, type '0') ");
+        while (!finish) {
+            System.out.println("Please enter the full path of file:(for return please press '0')");
             Scanner in = new Scanner(System.in);
-            String targetName = in.next();
-            if(targetName.equals("0")) {
+            String path = in.next();
+            if (!path.equals("0")) {
+                try {
+                    engine.readFile(path);
+                    System.out.println("File loaded successfully!");
+                    finish = true;
+                } catch (InvalidFileException e) {
+                    System.out.println("The file in: " + e.getPath() + "is invalid because" + e.getReason());
+                } catch (DependencyConflictException e) {
+                    System.out.println("There is a dependency conflict between Targets:"
+                            + e.getFirstTarget() + "," + e.getSecondTarget() + "dependency:" + e.getDependencyType());
+                } catch (DuplicateTargetsException e) {
+                    System.out.println("The target " + e.getTargetName() + "appears several times in the graph");
+                } catch (InvalidDependencyException e) {
+                    System.out.println("The dependency: " + e.getDependency() + " is not supported in the system");
+                } catch (TargetNotExistException e) {
+                    System.out.println("The target" + e.getName() + " does not exist");
+                }
+            } else {
                 finish = true;
             }
-            else {
+        }
+    }
+
+
+    private void printGraphInfo() {
+        GraphDTO graphDTO = null;
+        try {
+            graphDTO = engine.getGraphDTO();
+            System.out.println("Number of targets:" + graphDTO.getNumOfTargets());
+            System.out.println("Number of Leaves:" + graphDTO.getNumOfTargetsInPlace(PlaceInGraph.LEAF) +
+                    "Number of Middles:" + graphDTO.getNumOfTargetsInPlace(PlaceInGraph.MIDDLE) +
+                    "Number of Roots:" + graphDTO.getNumOfTargetsInPlace(PlaceInGraph.ROOT) +
+                    "Number of Independents:" + graphDTO.getNumOfTargetsInPlace(PlaceInGraph.INDEPENDENT));
+        } catch (NoFileInSystemException e) {
+            System.out.println("There is no file in the system");
+        }
+    }
+
+
+    private void printTarget() {
+        //TODO: ask Yael
+        //GraphDTO graphDTO = engine.getGraphDTO();
+        boolean finish = false;
+        while (!finish) {
+            System.out.println("Please enter the target's name:(to return to main menu, type '0')");
+            Scanner in = new Scanner(System.in);
+            String targetName = in.next();
+            if (targetName.equals("0")) {
+                finish = true;
+            } else {
                 try {
                     TargetDTO target = engine.getTarget(targetName);
-                    System.out.println("Target's name:" + target.getName());
-                    System.out.println("Target's place" + target.getPlace());
-                    if(!target.getDependsOn().isEmpty()) {
-                        System.out.println("dependsOn: ");
-                        for(TargetDTO targetDTO: target.getDependsOn()) {
-                            System.out.println("        "+ targetDTO.getName());
-                        }
-                    }
-                    else {
-                        System.out.println("There is no targets this target is depends on");
-                    }
-                    if(!target.getRequiredFor().isEmpty()) {
-                        System.out.println("requiredFor: ");
-                        for(TargetDTO targetDTO: target.getRequiredFor()) {
-                            System.out.println("        "+ targetDTO.getName());
-                        }
-                    }
-                    else {
-                        System.out.println("There is no targets this target is required for");
-                    }
-                    if(!target.getInfo().isEmpty()) {
-                        System.out.println("Target-info: " + target.getInfo());
-                    }
-                    else
-                    {
-                        System.out.println("There is not Target-info in this target");
-                    }
+                    printTargetDetails(target);
                 } catch (TargetNotExistException e) {
                     System.out.println("Target with name: " + e.getName() + "does not exist");
+                } catch (NoFileInSystemException e) {
+                    System.out.println("There is no file in the system");
                 }
             }
         }
     }
+
+    private void printTargetDetails(TargetDTO target) {
+        System.out.println("Target's name:" + target.getName());
+        System.out.println("Target's place" + target.getPlace());
+        if (!target.getDependsOn().isEmpty()) {
+            System.out.println("dependsOn: ");
+            for (TargetDTO targetDTO : target.getDependsOn()) {
+                System.out.println("        " + targetDTO.getName());
+            }
+        } else {
+            System.out.println("There is no targets this target is depends on");
+        }
+        if (!target.getRequiredFor().isEmpty()) {
+            System.out.println("requiredFor: ");
+            for (TargetDTO targetDTO : target.getRequiredFor()) {
+                System.out.println("        " + targetDTO.getName());
+            }
+        } else {
+            System.out.println("There is no targets this target is required for");
+        }
+        if (!target.getInfo().isEmpty()) {
+            System.out.println("Target-info: " + target.getInfo());
+        } else {
+            System.out.println("There is not Target-info in this target");
+        }
+
+    }
+
 }
