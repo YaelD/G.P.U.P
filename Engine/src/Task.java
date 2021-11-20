c763e6c72beb45c1807c931735b6d3eb49017d4e
Yael Davidov <yaeldv@mta.ac.il>
2021-11-20 19:21:36 +0200

    opening task class

--- Engine/src/Task.java
+++ Engine/src/Task.java
@@ -1,2 +1,22 @@
-public interface Task {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public abstract class Task {
+
+    protected Graph graph;
+
+    protected Task(Graph graph) {
+        this.graph = graph;
+    }
+
+    protected List<Target> TopologicalSort(){
+        List<Target> sortedTargets = new ArrayList<>();
+        Collection<Target> targets = this.graph.getTargets();
+        for(Target target : targets){
+
+        }
+        return sortedTargets;
+    }
+
 }
