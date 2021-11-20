4346c30e2bb06e334dfc5905c0bd6f23b19baccd
Yael Davidov <yaeldv@mta.ac.il>
2021-11-20 20:51:02 +0200

    creating task class

---
+++ Engine/src/SimulationTask.java
@@ -0,0 +1,11 @@
+public class SimulationTask extends Task{
+
+    public SimulationTask(Graph graph) {
+        super(graph);
+    }
+
+    @Override
+    protected void executeTask(Target target) {
+
+    }
+}
--- Engine/src/Task.java
+++ Engine/src/Task.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 public abstract class Task {
 
@@ -12,11 +10,44 @@ public abstract class Task {
 
     protected List<Target> TopologicalSort(){
         List<Target> sortedTargets = new ArrayList<>();
-        Collection<Target> targets = this.graph.getTargets();
-        for(Target target : targets){
+        Map<String, Integer> targetsInDegree = getTargetsInDegree();
+        Queue<Target> sourceTargets = new LinkedList<>();
 
+        for(Target target : this.graph.getTargets()){
+            if(target.getRequiredFor().isEmpty()){
+                sourceTargets.add(target);
+            }
         }
+        while(!sourceTargets.isEmpty()){
+            Target currTarget = sourceTargets.remove();
+            sortedTargets.add(currTarget);
+            for(Target neighborTarget : currTarget.getRequiredFor()){
+                targetsInDegree.put(neighborTarget.getName(), (targetsInDegree.get(neighborTarget.getName())-1));
+                if((targetsInDegree.get(neighborTarget.getName())) == 0){
+                    sourceTargets.add(neighborTarget);
+                }
+            }
+        }
+        //checkingForCycle(targetsInDegree);
         return sortedTargets;
     }
+
+    private void checkingForCycle(Map<String, Integer> targetsInDegree) {
+        for(Map.Entry<String,Integer> targetEntry : targetsInDegree.entrySet()){
+            if((targetEntry.getValue()) != 0){
+                //TODO: throw an exception for cycle
+            }
+        }
+    }
+
+    private Map<String, Integer> getTargetsInDegree(){
+        Map<String, Integer> targetsInDegree = new HashMap<>();
+        for(Map.Entry<String,Target> targetEntry : this.graph.getTargetGraph().entrySet()){
+            targetsInDegree.put(targetEntry.getKey(), targetEntry.getValue().getRequiredFor().size());
+        }
+        return targetsInDegree;
+    }
+
+    protected abstract void executeTask(Target target);
 
 }
