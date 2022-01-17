19d1f3f30db2c5d77f7069e62f7dac417f545137
Yael Davidov <yaeldv@mta.ac.il>
2022-01-17 11:57:51 +0200

    cycle fxml changes

--- GraphicalUI/src/findcycles/FindCyclesController.java
+++ GraphicalUI/src/findcycles/FindCyclesController.java
@@ -1,5 +1,7 @@
 package findcycles;
 
+import dto.TargetDTO;
+import engine.Engine;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -7,6 +9,8 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ListView;
 
 public class FindCyclesController {
+
+    private Engine engine;
 
     @FXML
     private ChoiceBox<String> targetsChoiceBox;
@@ -22,5 +26,14 @@ public class FindCyclesController {
 
     }
 
+    private void initTargetsChoiceBox(){
+        for(TargetDTO targetDTO: this.engine.getGraphDTO().getTargets().values()){
+            this.targetsChoiceBox.getItems().add(targetDTO.getName());
+        }
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
 }
 
