71693d20c5d5395ecb466460714d64bf32bef073
Matan <matanp103@gmail.com>
2022-01-19 21:35:31 +0200

    Work on GUI- Run Task

--- GraphicalUI/src/runtask/ChooseTaskController.java
+++ GraphicalUI/src/runtask/ChooseTaskController.java
@@ -5,22 +5,32 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
 
 public class ChooseTaskController {
 
     @FXML private Button continueButton;
     @FXML private ChoiceBox<Integer> numOfThreadsCB;
+    @FXML private RadioButton simulationRadioButton;
     @FXML private ToggleGroup taskToggleGroup;
+    @FXML private RadioButton compilationRadioButton;
+    @FXML private RadioButton fromScratchRadioButton;
     @FXML private ToggleGroup runTypeToggle;
+    @FXML private RadioButton incrementalRadioButton;
+
+
     private Engine engine;
 
     @FXML
     void enableNextPanel(ActionEvent event) {
-        if(!checkValidation()){
-            //TODO:show warning label
-        }
-        else if(taskToggleGroup.getSelectedToggle().)
+
     }
 
     @FXML
