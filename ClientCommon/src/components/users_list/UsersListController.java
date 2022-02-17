dcb57aa49de8c09beb2150d40712531d12102217
Yael Davidov <yael.davidov97@gmail.com>
2022-02-17 16:20:34 +0200

    work on ClientCommon module

--- ClientCommon/src/components/users_list/UsersListController.java
+++ ClientCommon/src/components/users_list/UsersListController.java
@@ -8,7 +8,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
